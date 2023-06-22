3787d1826d28dbf69e63aa07bf5512b9207291d1
s2pakvitols <s2pakvitols@gmail.com>
2023-06-22 20:03:51 +0200

    end point 22.06

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
 // Add imports above this line
@@ -7,3 +7,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryItemEl = document.querySelector('.gallery');
+function createGalleryMarkUp(items) {
+  return items
+    .map(
+      item =>
+        `<li class="gallery__item">
+   <a class="gallery__link" 
+   href ="${item.original}">
+   <img
+   class="gallery__image" 
+   src="${item.preview}"
+   alt="${item.description}"/></a></li>`
+    )
+    .join(' ');
+}
+const addGalleryMarkUp = createGalleryMarkUp(galleryItems);
+galleryItemEl.innerHTML = addGalleryMarkUp;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  enableKeyboard: true,
+  close: true,
+  
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_STORAGE = 'videoplayer-current-time';
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(data.seconds));
+  }),
+  1000
+);
+const getTime = localStorage.getItem(KEY_STORAGE);
+if (getTime) {
+  player.setCurrentTime(JSON.parse(getTime));
+}
+
+player
+  .setCurrentTime()
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
